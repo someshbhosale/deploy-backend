4c548b99476c1d49b35cb7aa17eb762bba906a2e
Somesh Bhosale <someshbhosale2@gmail.com>
2024-06-02 01:49:28 +0530

    deploy

--- app.js
+++ app.js
@@ -43,11 +43,9 @@ app.get('/api/results', async (req, res) => {
   res.json(results);
 });
 
-module.exports = app;
 const port = process.env.PORT || 3000;
 app.get("/",(req,res) =>{
     res.send("server is running");
 })
 
 app.listen(port, console.log(`server is running on port ${port}`));
-
